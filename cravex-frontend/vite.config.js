2077a127228d86944f7da6a14ee69303130ec0dc
nitiraj-king <nitirajsinghsolanki1403@gmail.comm>
2025-10-23 12:09:42 +0530

    some vite config change for vercel

--- cravex-frontend/vite.config.js
+++ cravex-frontend/vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist", // ✅ This is where Vercel will look for your built files
+  },
+  server: {
+    port: 5173, // optional - for local dev
+  },
 });
