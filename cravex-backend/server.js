fa2ff66b183bb60359a9f04d4d36ac9373fcd6bd
nitiraj-king <nitirajsinghsolanki1403@gmail.comm>
2025-10-23 17:18:49 +0530

    port update

--- cravex-backend/server.js
+++ cravex-backend/server.js
@@ -46,8 +46,8 @@ app.get('/', (req, res) => {
   res.send('CraveX Website is ON Now!')
 });
 
-app.listen( PORT , () => {
-  console.log('Server is running on port 5000 :  http://localhost:5000');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 
